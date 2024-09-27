051af869ca37ebb1040e687e1c0ba90adcc6cf17
Mateo <cuellamateo@gmail.com>
2024-09-27 16:25:36 -0300

    se realizo la lógica para la creación de un nuevo usuario en la página de registro

--- client/src/register.js
+++ client/src/register.js
@@ -1,3 +1,31 @@
-// ! REALIZAR LA LÓGICA DE REGISTRO DE USUARIOS AQUÍ
+import './style.css'
 
-import "./style.css";
+// Obtener el formulario de inicio de sesión
+const registerForm = document.getElementById('register-form')
+
+registerForm.addEventListener('submit', async (e) => {
+  // Evitar que el formulario recargue la página
+  e.preventDefault()
+
+  // Crear un objeto FormData con los datos del formulario
+  const formData = new FormData(registerForm)
+
+  // Convertir el objeto FormData a un objeto plano
+  const entries = Object.fromEntries(formData.entries())
+
+  // Realizar una solicitud POST a la API de registro de usuarios
+  fetch('http://localhost:4321/auth/sign-up', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify(entries),
+  }).then((response) => {
+    if (response.ok) {
+      window.location.href = '/'
+    } else {
+      return
+    }
+  })
+})
